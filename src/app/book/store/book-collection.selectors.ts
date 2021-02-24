962968afe3d361393d7e7ece2841b563389e76bd
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-02-24 21:53:44 +0100

    solve--ngrx-use-entity

--- src/app/book/store/book-collection.selectors.ts
+++ src/app/book/store/book-collection.selectors.ts
@@ -1,8 +1,14 @@
+import { createEntityAdapter } from '@ngrx/entity';
 import { createSelector } from '@ngrx/store';
 import { selectRouteParam } from '@store/router';
+import { Book } from '../models';
 import { bookFeature } from './book.feature';
 
-export const bookCollection = createSelector(bookFeature, slice => slice.bookCollection.entities);
+const adapter = createEntityAdapter<Book>();
+
+const bookCollectionSelector = createSelector(bookFeature, slice => slice.bookCollection);
+
+export const { selectAll: bookCollection } = adapter.getSelectors(bookCollectionSelector);
 
 export const bookByIsbn = createSelector(selectRouteParam('isbn'), bookCollection, (isbn, books) =>
   books.find(book => book.isbn === isbn)
