06651d19ad52c75cafcd6b99145094a63855ccdb
Gregor Woiwode <gregor.woiwode@gmail.com>
2021-02-24 21:38:19 +0100

    solve--ngrx-use-router-store

--- src/app/book/store/book-collection.selectors.ts
+++ src/app/book/store/book-collection.selectors.ts
@@ -1,7 +1,9 @@
 import { createSelector } from '@ngrx/store';
+import { selectRouteParam } from '@store/router';
 import { bookFeature } from './book.feature';
 
 export const bookCollection = createSelector(bookFeature, slice => slice.bookCollection.entities);
 
-export const bookByIsbn = (isbn: string) =>
-  createSelector(bookCollection, books => books.find(book => book.isbn === isbn));
+export const bookByIsbn = createSelector(selectRouteParam('isbn'), bookCollection, (isbn, books) =>
+  books.find(book => book.isbn === isbn)
+);
