2ea52f5af4a8c2bda4d3f42e4d662a847b2d0eb1
web-dave <dave@webdave.de>
2023-05-12 13:07:24 +0200

    erster Test

--- src/app/book/book-card/book-card.component.spec.ts
+++ src/app/book/book-card/book-card.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookCardComponent } from './book-card.component';
@@ -8,17 +9,26 @@ describe('BookCardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [BookCardComponent]
+      declarations: [BookCardComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(BookCardComponent);
     component = fixture.componentInstance;
+    // fixture.debugElement.nativeElement
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fall back to "na" on default', () => {
+    const na = 'n/a';
+    const myComp = new BookCardComponent();
+    const book = myComp.content;
+    expect(book.title).toBe(na);
   });
 });
