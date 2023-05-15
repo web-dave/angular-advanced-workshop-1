bb0ff605b6692959356506b5e5416755b5fb0aef
web-dave <dave@webdave.de>
2023-05-15 11:54:48 +0200

    RouterTestingModule

--- src/app/book/book-card/book-card.component.spec.ts
+++ src/app/book/book-card/book-card.component.spec.ts
@@ -1,5 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { bookNa } from '../models';
 
 import { BookCardComponent } from './book-card.component';
@@ -12,7 +13,8 @@ describe('BookCardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BookCardComponent],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [RouterTestingModule]
     }).compileComponents();
   });
 
