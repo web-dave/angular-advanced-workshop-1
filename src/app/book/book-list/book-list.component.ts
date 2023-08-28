689fb6bbe8fe7ce3237391a529a348f043f87ea0
Dave <dave@webdave.de>
2023-08-28 10:28:34 +0200

    Select

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BookApiService } from '../book-api.service';
 import { Book } from '../models';
+import { Store } from '@ngrx/store';
+import { BookCollectionSlice } from '../store/book-collection.slice';
+import { bookFeatureName } from '../store/book.feature';
 
 @Component({
   selector: 'ws-book-list',
@@ -9,9 +12,10 @@ import { Book } from '../models';
   templateUrl: 'book-list.component.html'
 })
 export class BookListComponent {
-  books$: Observable<Book[]>;
-
-  constructor(private bookData: BookApiService) {
-    this.books$ = this.bookData.getAll();
+  books$: Observable<ReadonlyArray<Book>>;
+  // <{ [bookFeatureName]: BookCollectionSlice }>
+  constructor(private bookData: BookApiService, private store: Store) {
+    // eslint-disable-next-line ngrx/prefer-selector-in-select
+    this.books$ = this.store.select((state: any) => state[bookFeatureName].entities);
   }
 }
